ca840b534a12cd17d4683c1713dffc3530ea9b43
Quintony1 <nyashajerahuni@gmail.com>
2022-07-12 04:04:01 -0700

    write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -9,7 +9,7 @@
  * @width: get width.
  * @precision: precision specifier
  * @size: Size specifier
- *
+ * Author Vincent and Nyasha
  * Return: Number of chars printed.
  */
 int handle_write_char(char c, char buffer[],
